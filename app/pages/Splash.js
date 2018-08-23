7b737fd095e104cd76e0aceb21c96c1ad8c33cc5
smallLabel <1007243939@qq.com>
2018-08-23 18:00:22 +0800

    新增splash页面

---
+++ app/pages/Splash.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { View, StyleSheet } from 'react-native';
+
+
+export default class Splash extends Component {
+    render() {
+        return (
+           <View style={{backgroundColor:'red'}}></View>
+        );
+    }
+}
