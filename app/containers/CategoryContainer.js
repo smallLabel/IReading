2939e0c2a5df99426c85e0ca2b748a4e6367b01d
smallLabel <1007243939@qq.com>
2018-08-23 16:30:55 +0800

    创建首页和分类的导航控制器

---
+++ app/containers/CategoryContainer.js
@@ -0,0 +1,10 @@
+import React ,{ Component } from 'react';
+import Category from '../pages/Category/Category';
+
+export default class CategoryContainer extends Component {
+    render() {
+        return (
+            <Category>category</Category>
+        );
+    }
+}
---
+++ app/containers/MainContainer.js
@@ -0,0 +1,10 @@
+import React,{ Component } from 'react';
+import Main from '../pages/Main/Main';
+
+export default class MainContainer extends Component {
+    render() {
+        return (
+            <Main>main</Main>
+        );
+    }
+}
