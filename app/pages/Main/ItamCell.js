28cd2581a9fe9b28fe3240868f53593018b12dfb
smallLabel <1007243939@qq.com>
2018-08-26 23:47:46 +0800

    新增moment依赖项 修改itemcell的对象创建方法

--- app/pages/Main/ItamCell.js
+++ app/pages/Main/ItamCell.js
@@ -1,3 +1,13 @@
 import React, {Component} from 'react';
 import {View, StyleSheet, Image, Text, TouchableOpacity} from 'react-native'
+import moment from 'moment';
+require("moment/locale/zh-cn");
 
+
+const  ItemCell = ({article, onPressHandle})=>(
+    <TouchableOpacity onPress={onPressHandle(article)}>
+        <View>
+
+        </View>
+    </TouchableOpacity>
+);
