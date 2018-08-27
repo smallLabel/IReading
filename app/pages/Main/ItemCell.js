0f0ebc35b2e216d8b27606bda803632ad67b5be1
smallLabel <1007243939@qq.com>
2018-08-27 11:11:44 +0800

    设计itemcell结构

--- app/pages/Main/ItemCell.js
+++ app/pages/Main/ItemCell.js
@@ -6,10 +6,54 @@ require("moment/locale/zh-cn");
 
 const  ItemCell = ({article, onPressHandle})=>(
     <TouchableOpacity onPress={onPressHandle(article)}>
-        <View>
-
+        <View style={styles.containerItem}>
+            <Image style={styles.itemImage}/>
+            <View style={styles.itemRightContent}>
+                <Text style={styles.title}></Text>
+                <View style={styles.itemRightBottom}>
+                    <Text style={styles.userName}></Text>
+                    <Text style={}></Text>
+                </View>
+            </View>
         </View>
     </TouchableOpacity>
 );
+
+const  styles = StyleSheet.create({
+    containerItem:{
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor:'#fcfcfc',
+        padding:10,
+        borderBottomColor: '#ddd',
+        borderBottomWidth: 1
+    },
+    title: {
+        fontSize: 18,
+        textAlign: 'left',
+        color: 'black'
+    },
+    itemImage:{
+        width:88,
+        height: 65,
+        marginRight: 10
+    },
+    itemRightContent: {
+        flex:1,
+    },
+    itemRightBottom:{
+        flexDirection: 'row',
+        flex: 1,
+        alignItems: 'center'
+    },
+    userName:{
+        flex:1,
+        fontSize: 14,
+        color:'#87CEFA',
+        marginTop: 5,
+        marginRight: 5
+    }
+})
 
 export  default  ItemCell;
